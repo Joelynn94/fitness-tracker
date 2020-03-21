995a16281c6270a177b434f45a19183f117123d7
Joe Lynn <josephlynn94@gmail.com>
2020-03-21 02:05:58 -0400

    added update api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const Workout = require("../models/workout")
 
 module.exports = function(app) {
+  // toute to get all of the workouts
   app.get("/api/workouts", (req, res) => {
     Workout.find({})
       .then((results) => {
@@ -11,6 +12,7 @@ module.exports = function(app) {
       })
   })
 
+  // route to post a new workout
   app.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
       .then(results => {
@@ -21,17 +23,24 @@ module.exports = function(app) {
       })
   })
 
-  // app.get("/api/workouts/:id", ({ params }, res) => {
-  //   Workout.findOneAndUpdate({ _id : params.id })
-  //     .then(results => {
-  //       res.json(results)
-  //       console.log({_id : params.id})
-  //     })
-  //     .catch(err => {
-  //       res.status(400).json(err)
-  //     })
-  // })
+  // route to update a workout
+  app.put("/api/workouts/:id", ({ params, body }, res) => {
+    // find the id to update the database 
+    Workout.findByIdAndUpdate({ _id : params.id }, body)
+      .then(() => {
+        // have to do another .then to return a promise that updates the front-end
+        Workout.findOne({ _id: params.id })
+        .then(results => {
+          res.json(results)
+          console.log({_id : params.id})
+        })
+      })
+      .catch(err => {
+        res.status(400).json(err)
+      })
+  })
 
+  // route to get the stats 
   app.get("/api/workouts/range", (req, res) => {
     Workout.find({})
       .then((results) => {
