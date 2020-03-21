39053e50feaa2d464ec6dedab1853ad8b1e87e1f
Joe Lynn <josephlynn94@gmail.com>
2020-03-20 23:30:38 -0400

    changed the workout model

--- models/workout.js
+++ models/workout.js
@@ -1,39 +1,42 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  workoutType: {
-    type: String,
-    trim: true,
-    required: "Enter a workour type"
-  },
-  workoutName: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  duration: {
-    type: Number,
-    required: "Enter a duration"
-  },
-  weight: {
-    type: Number,
-    required: "Enter the weight you used"
-  },
-  reps: {
-    type: Number,
-    required: "Enter the amount of reps you did"
-  },
-  sets: {
-    type: Number,
-    required: "Enter the amount of sets you completed"
-  },
-  userCreated: {
+  day: {
     type: Date,
     default: Date.now()
-  }
-})
+  },
+  exercises: [
+    {
+      workoutType: {
+        type: String,
+        trim: true,
+        required: "Enter a workour type"
+      },
+      workoutName: {
+        type: String,
+        trim: true,
+        required: true
+      },
+      duration: {
+        type: Number,
+        required: "Enter a duration"
+      },
+      weight: {
+        type: Number,
+        required: "Enter the weight you used"
+      },
+      reps: {
+        type: Number,
+        required: "Enter the amount of reps you did"
+      },
+      sets: {
+        type: Number,
+        required: "Enter the amount of sets you completed"
+      }
+    }
+  ]
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // using mongoose to connect to database
-mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/workoutsDB", { useNewUrlParser: true, useFindAndModify: false});
+mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false});
 
 // bringing in the routes folder 
 app.use(require("./routes/api.js"));
